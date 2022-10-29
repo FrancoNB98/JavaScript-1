eda859aaf42786b2d0ae502c27bf994b57be4974
Franco Benitez <francobenitez1798@gmail.com>
2022-10-28 21:26:54 -0300

    primera pre-entrega

---
+++ script.js
@@ -0,0 +1,67 @@
+// Calcular costo total de productos y/o servicios seleccionados por el usuario..
+
+
+let usuario = prompt("Ingrese su nombre")
+alert("Bienvenido " + usuario)
+
+let seleccionar = prompt("Seleccionar Productos que desea\n-Teclado\n-Mouse\n-Auriculares")
+let cantidadProductos = parseInt(prompt("Seleccione la cantidad que desea comprar. Maximo 3 productos"))
+let cantidad = cantidadProductos
+
+let producto1 = ("Teclado").toLowerCase()
+let producto2 = ("Mouse").toLowerCase()
+let producto3 = ("Auriculares").toLowerCase()
+
+switch (cantidad) {
+    case 1:
+        if(seleccionar == producto1)
+        console.log(producto1 + " " + "= $12.600")
+        break;
+    case 2:
+        if(seleccionar == producto1)
+        console.log(producto1 + " " + "= $25.200")
+        break;
+    case 3:
+        if(seleccionar == producto1)
+        console.log(producto1 + " " + "= $37.800")
+        break;
+    default:(cantidad > 3)
+        console.log("Selecciono mas productos del permitido")
+        break;
+}
+
+switch (cantidad) {
+    case 1:
+        if(seleccionar == producto2)
+        console.log(producto2 + " " + "= $7.500")
+        break;
+    case 2:
+        if(seleccionar == producto2)
+        console.log(producto2 + " " + "= $15.000")
+        break;
+    case 3:
+        if(seleccionar == producto2)
+        console.log(producto2 + " " + "= $22.500")
+        break;
+    default:(cantidad > 3)
+        console.log("Selecciono mas productos del permitido")
+        break;
+}
+
+switch (cantidad) {
+    case 1:
+        if(seleccionar == producto3)
+        console.log(producto3 + " " + "= $19.500")
+        break;
+    case 2:
+        if(seleccionar == producto3)
+        console.log(producto3 + " " + "= $39.000")
+        break;
+    case 3:
+        if(seleccionar == producto3)
+        console.log(producto2 + " " + "= $58.500")
+        break;
+    default:(cantidad > 3)
+        console.log("Selecciono mas productos del permitido")
+        break;
+}
